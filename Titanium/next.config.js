6cc370ab2c37d49225d3e07ccae22e7a5f18b021
shubh005-Debug <sp540793@gmail.com>
2025-03-27 13:56:58 +0530

    Add files via upload

---
+++ Titanium/next.config.js
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  env: {
+    MONGODB_URI: process.env.MONGODB_URI,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': require('path').resolve(__dirname, 'src'),
+    };
+    return config;
+  }
+}
+
+module.exports = nextConfig
